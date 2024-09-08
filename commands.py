cfbaf493f1f36faa5d10497a462522b32bcb6e0f
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-08 21:59:22 +0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -12,6 +12,7 @@ async def set_commands():
       BotCommand('convertvcf', 'Mengonversi file dari format XLSX ke VCF'),
       BotCommand('convertvcf_to_txt', 'mengonversi file dari format vcf ke txt'),
       BotCommand('gabungvcf', 'Menggabungkan beberapa file vcf menjadi 1'),
+      BotCommand('chat_to_vcf', 'Mengonversi chat ke vcf '),
       BotCommand('cancel', 'Membatalkan proses'),
     ],
   )
