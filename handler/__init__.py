cb4f2a03c0e81d53182c50880a19b4fe1743a2f4
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-08 16:37:15 +0700

    Delete handler directory

--- handler/__init__.py
+++
@@ -1,7 +0,0 @@
-from .start import *
-from .convert import *
-from .pecahtxt import *
-from .pecahvcf import *
-from .convertvcf import *
-from .convertxlsx import *
-from .convertvcf_to_txt import *
