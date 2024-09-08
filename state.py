ae34d19f98ad633ad232f6ac83316a85989da06f
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-08 09:50:50 +0700

    Update state.py

--- state.py
+++ state.py
@@ -1,31 +1,35 @@
 from telebot.asyncio_handler_backends import State, StatesGroup
 
 class ConvertState(StatesGroup):
-  filename = State()
-  name = State()
-  cname = State()
-  totalc = State()
-  totalf = State()
+    filename = State()
+    name = State()
+    cname = State()
+    totalc = State()
+    totalf = State()
 
 class ConvertVcfState(StatesGroup):
-  filename = State()
-  name = State()
-  cname = State()
-  totalc = State()
-  totalf = State()
+    filename = State()
+    name = State()
+    cname = State()
+    totalc = State()
+    totalf = State()
 
 class ConvertXlsxState(StatesGroup):
-  filename = State()
-  name = State()
+    filename = State()
+    name = State()
 
 class PecahTxtState(StatesGroup):
-  filename = State()
-  name = State()
-  totaln = State()
-  totalf = State()
+    filename = State()
+    name = State()
+    totaln = State()
+    totalf = State()
 
 class PecahVcfState(StatesGroup):
-  filename = State()
-  name = State()
-  totalc = State()
-  totalf = State()
+    filename = State()
+    name = State()
+    totalc = State()
+    totalf = State()
+
+class ConvertVcfToTxtState(StatesGroup):
+    filename = State()
+    name = State()
