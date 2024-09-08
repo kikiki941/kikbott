5bf31de13fb406d658758bda4b38e0437111870a
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-08 10:25:16 +0700

    Add files via upload

---
+++ handlers/__init__.py
@@ -0,0 +1,7 @@
+from .start import *
+from .convert import *
+from .pecahtxt import *
+from .pecahvcf import *
+from .convertvcf import *
+from .convertxlsx import *
+from .convertvcf_to_txt import *
