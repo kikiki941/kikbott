b507e92282fa2bd838388076059d11201a1cbd55
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-09 20:25:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telebot.asyncio_filters import *
 from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_storage import StateMemoryStorage
 
-owner = [6559095531]
+owner = [6203222756]
 whitelist = {}
 datetime_format = f"%H:%M:%S %d-%m-%Y"
 wib = timezone('Asia/Jakarta')
