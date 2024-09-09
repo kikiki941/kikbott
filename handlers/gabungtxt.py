cdaf7d35154d4b8d2e0dbd8bcae33152d9a8904e
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-09 19:15:37 +0700

    Update gabungtxt.py

--- handlers/gabungtxt.py
+++ handlers/gabungtxt.py
@@ -16,6 +16,11 @@ if not os.path.exists('files'):
 @bot.message_handler(commands='gabungtxt')
 async def gabung_txt_command(message: Message):
     try:
+        access = check_user(whitelist, message.from_user.id)
+    
+    if not access:
+      return await bot.send_message(message.chat.id, txt_pesan_vip)
+        
         await bot.delete_state(message.from_user.id, message.chat.id)
         await bot.set_state(message.from_user.id, GabungTxtState.waiting_for_files, message.chat.id)
         await bot.reply_to(message, txt_gabung_txt)
