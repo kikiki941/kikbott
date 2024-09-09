75ed7d28dd525f7f2b5c5dbf24a5ee77149a1ee1
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-09 19:12:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
-from telebot import logger
-from telebot.async_telebot import AsyncTeleBot
-from telebot.asyncio_filters import *
-from telebot.asyncio_storage import StateMemoryStorage
+from pytz import timezone
 from logging import ERROR
+from telebot import logger
+from telebot.asyncio_filters import *
+from telebot.async_telebot import AsyncTeleBot
+from telebot.asyncio_storage import StateMemoryStorage
 
-bot_token = '7370223134:AAFydWlZPdTHP5q4C43lHKDCjMIBwPCVIzE'
+owner = [6559095531,6203222756]
+whitelist = {}
+datetime_format = f"%H:%M:%S %d-%m-%Y"
+wib = timezone('Asia/Jakarta')
+bot_token = '7432689433:AAFRWu1F_DksP8CxQzwXWLaGStLOgkOLta0'
 bot = AsyncTeleBot(bot_token, parse_mode='HTML', state_storage=StateMemoryStorage())
 
 bot.add_custom_filter(StateFilter(bot))
