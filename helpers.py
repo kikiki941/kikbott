61231da06ca437c139ebbca20082fcc204453be7
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-09 13:06:18 +0700

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -251,6 +251,26 @@ def extract_password(output):
         return password_match.group(1).strip()
     return None
 
+def pecah_txt(data: Dict[str, Any]) -> list:
+  
+    numbers = check_number(data['filename'])
+    split_number = split(numbers, data['totaln'])
+    countf = 0
+    files = []
+
+    for numbers in split_number:
+        countf += 1
+        txt_name = f"files/{data['name']}_{countf}.txt"
+        files.append(txt_name)
+
+        with open(txt_name, 'w', encoding='utf-8') as file:
+            for number in numbers:
+                file.write(number + "\n")
+
+        if countf == data['totalf']:
+            break
+
+    return files
 
 def split(arr, num):
     return [arr[x:x+num] for x in range(0, len(arr), num)]
