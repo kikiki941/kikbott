7f6975e9f51d81c0de707904513b4331eeeb1830
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-14 08:43:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ owner = [6203222756]
 whitelist = {}
 datetime_format = f"%H:%M:%S %d-%m-%Y"
 wib = timezone('Asia/Jakarta')
-bot_token = '7370223134:AAFydWlZPdTHP5q4C43lHKDCjMIBwPCVIzE'
+bot_token = '7013710425:AAFhyxnBqMTcvEJ9jjNfiVad_NFH0Aufwag'
 bot = AsyncTeleBot(bot_token, parse_mode='HTML', state_storage=StateMemoryStorage())
 
 bot.add_custom_filter(StateFilter(bot))
