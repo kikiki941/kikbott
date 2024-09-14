76b929739eb271d9d8d459fe22ab198b918b4b67
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-14 17:21:53 +0700

    Update and rename convert2 to convert2.py

--- handlers/convert2.py
+++ handlers/convert2.py
@@ -7,7 +7,7 @@ from telebot.apihelper import ApiTelegramException
 
 from bot import bot
 from message import *
-from helpers import *
+from helpers import convert2
 from state import Convert2State
 
 @bot.message_handler(commands='convert2')
