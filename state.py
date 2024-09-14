b2adc8b8811fd592e8a23dc9be5f880f400625a7
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-14 17:18:38 +0700

    Update state.py

--- state.py
+++ state.py
@@ -60,3 +60,15 @@ class VipState(StatesGroup):
 
 class kolomState(StatesGroup):
     waiting_for_file = State()
+
+class Convert2State(StatesGroup):
+    filename = State()  # Menunggu pengguna mengirimkan file txt
+    name = State()      # Menunggu pengguna memasukkan nama file vcf
+    cname = State()     # Menunggu pengguna memasukkan nama kontak
+    totalc = State()    # Menunggu pengguna memasukkan jumlah kontak per file
+    totalf = State()    # Menunggu pengguna memasukkan jumlah file
+    change_name_prompt = State()  # Menunggu pengguna memilih apakah nama file akan berganti
+    change_every = State()  # Menunggu input jumlah file sebelum nama berganti
+    change_limit = State()  # Menunggu input batas pergantian nama
+    new_name_1 = State()  # Menunggu input nama file baru
+    new_name_2 = State()  # Menunggu input nama file baru berikutnya
