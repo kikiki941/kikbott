a20dd6601210114313e21744ee9b081ca8f5579c
RAT <158288653+Surya26668@users.noreply.github.com>
2024-09-19 19:32:58 +0700

    Update convert.py

--- handlers/convert.py
+++ handlers/convert.py
@@ -7,7 +7,7 @@ from telebot.apihelper import ApiTelegramException
 
 from bot import bot
 from message import *
-from helpers import convert
+from helpers import *
 from state import ConvertState
 
 @bot.message_handler(commands='convert')
@@ -15,9 +15,9 @@ async def convert_command(message):
     try:
         await bot.delete_state(message.from_user.id, message.chat.id)
         await bot.set_state(message.from_user.id, ConvertState.filename, message.chat.id)
-        await bot.reply_to(message, "Silakan kirim file .txt untuk memulai konversi.")
+        await bot.reply_to(message, txt_convert)
     except Exception as e:
-        logging.error("Error in convert_command: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
 
 @bot.message_handler(state=ConvertState.filename, content_types=['document'])
 async def txt_get(message: Message):
@@ -38,14 +38,16 @@ async def txt_get(message: Message):
 
         await bot.send_message(message.chat.id, 'File diterima. Silakan masukkan nama file vcf yang akan dihasilkan:')
     except Exception as e:
-        logging.error("Error in txt_get: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
 
 @bot.message_handler(state=ConvertState.filename)
 async def not_txt(message: Message):
     try:
+        if message.text.startswith('/'):
+            await bot.delete_state(message.from_user.id, message.chat.id)
         await bot.send_message(message.chat.id, 'Kirim file .txt')
     except Exception as e:
-        logging.error("Error in not_txt: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
 
 @bot.message_handler(state=ConvertState.name)
 async def name_get(message: Message):
@@ -55,7 +57,7 @@ async def name_get(message: Message):
         async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['name'] = message.text
     except Exception as e:
-        logging.error("Error in name_get: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
 
 @bot.message_handler(state=ConvertState.cname)
 async def cname_get(message: Message):
@@ -65,7 +67,7 @@ async def cname_get(message: Message):
         async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['cname'] = message.text
     except Exception as e:
-        logging.error("Error in cname_get: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
 
 @bot.message_handler(state=ConvertState.totalc, is_digit=True)
 async def totalc_get(message: Message):
@@ -75,31 +77,16 @@ async def totalc_get(message: Message):
         async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['totalc'] = int(message.text)
     except Exception as e:
-        logging.error("Error in totalc_get: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
 
 @bot.message_handler(state=ConvertState.totalf, is_digit=True)
 async def totalf_get(message: Message):
     try:
-        await bot.send_message(message.chat.id, f'Jumlah file diatur menjadi: {message.text}. Apakah penomoran akan di-reset setiap file? (ya/tidak)')
-        await bot.set_state(message.from_user.id, ConvertState.reset_numbering, message.chat.id)
+        await bot.send_message(message.chat.id, f'Jumlah file diatur menjadi: {message.text}. Mulai mengonversi...')
         async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['totalf'] = int(message.text)
-    except Exception as e:
-        logging.error("Error in totalf_get: ", exc_info=True)
-
-@bot.message_handler(state=ConvertState.reset_numbering)
-async def reset_numbering_get(message: Message):
-    try:
-        if message.text.lower() not in ['ya', 'tidak']:
-            return await bot.send_message(message.chat.id, "Masukkan jawaban 'ya' atau 'tidak'.")
-        
-        await bot.send_message(message.chat.id, 'Mulai mengonversi...')
-        async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-            data['reset_numbering'] = message.text.lower() == 'ya'
             vcf_files = convert(data)
             os.remove(data['filename'])
-            
-            # Mengirim file VCF ke pengguna
             for file in vcf_files:
                 try:
                     await bot.send_document(message.chat.id, open(file, 'rb'))
@@ -116,7 +103,7 @@ async def reset_numbering_get(message: Message):
             await bot.send_message(message.chat.id, "Convert selesai!")
         await bot.delete_state(message.from_user.id, message.chat.id)
     except Exception as e:
-        logging.error("Error in reset_numbering_get: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
 
 @bot.message_handler(state=ConvertState.totalc, is_digit=False)
 @bot.message_handler(state=ConvertState.totalf, is_digit=False)
@@ -124,4 +111,4 @@ async def invalid_input(message: Message):
     try:
         await bot.send_message(message.chat.id, 'Masukkan angka yang valid.')
     except Exception as e:
-        logging.error("Error in invalid_input: ", exc_info=True)
+        logging.error("error: ", exc_info=True)
