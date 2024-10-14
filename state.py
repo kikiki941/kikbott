168420d1b8ceed6f2087d8da0e11393ccb20508d
RAT <158288653+Surya26668@users.noreply.github.com>
2024-10-14 13:18:23 +0700

    Update state.py

--- state.py
+++ state.py
@@ -74,18 +74,6 @@ class VipState(StatesGroup):
 class kolomState(StatesGroup):
     waiting_for_file = State()
 
-class Convert2State(StatesGroup):
-    filename = State()  # Menunggu input file .txt
-    name = State()      # Menunggu input nama file
-    cname = State()     # Menunggu input nama kontak
-    totalc = State()    # Menunggu input jumlah kontak per file
-    totalf = State()    # Menunggu input jumlah file
-    change_name_prompt = State()  # Menunggu input apakah nama file akan berganti
-    change_every = State()        # Menunggu input setiap berapa file nama akan berganti
-    change_limit = State()        # Menunggu input batas pergantian nama
-    new_name_1 = State()          # Menunggu input nama file baru
-    contact_names = State()     
-
 class HitungCtcState(StatesGroup):
     waiting_for_files = State()  # State where the bot is waiting for .vcf files
 
